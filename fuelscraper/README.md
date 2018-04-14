7f19a664d283e7c4a68b91b8c23cab1bcc70a1bf
Dan <dans.address@outlook.com>
2018-04-14 20:39:10 +0000

    Create main.py, change folder structure, README
    main.py
    + Able to return html table from the query response
    - Minimal error checking

---
+++ fuelscraper/README.md
@@ -0,0 +1,1 @@
+# Python Meetup Group Fuelwatch Scraper
---
+++ fuelscraper/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+from json2html import *
+
+from fuelwatcher import FuelWatch
+
+api = FuelWatch()
+
+"""
+Fuelwatch RSS XML Scraper that outputs a html file containing a
+html table with the results of query.
+
+Requires the fuelwatcher module which parses the raw XML into JSON format.
+"""
+
+
+def main():
+    json_ = setup()
+    html = test(json_)
+    save_output(html)
+
+
+def setup():
+    """Query the endpoint and return JSON."""
+    api.query(suburb='Floreat')
+    json_ = api.get_json
+    return json_
+
+
+def test(resp):
+    """Pass in JSON and return html table."""
+    return json2html.convert(json=resp)
+
+
+def save_output(html):
+    with open('test.html', 'w') as f:
+        for line in html:
+            f.write(line)
+
+
+if __name__ == '__main__':
+    main()
