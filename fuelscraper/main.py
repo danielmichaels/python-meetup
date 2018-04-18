7bda3b94ab27d584023bcc12091be93fd73c8063
Dan <dans.address@outlook.com>
2018-04-18 14:00:22 +0000

    Change naming conventions. Add better doc strings.

--- fuelscraper/main.py
+++ fuelscraper/main.py
@@ -15,24 +15,44 @@ Requires the fuelwatcher module which parses the raw XML into JSON format.
 
 def main():
     json_ = setup()
-    html = test(json_)
-    save_output(html)
+    resp = json_to_html(json_)
+    save_output(resp)
 
 
 def setup():
-    """Query the endpoint and return JSON."""
+    """Query the endpoint and return JSON.
+
+    Variables accepted by `query()`:
+
+    def query(self, product: int = None, suburb: str = None, region: int = None, 
+            brand: int = None, surrounding: str = None, day: str = None):
+
+    Example:
+
+    api.query(product=4, region=25, day='yesterday')
+
+    :return json formatted query response.
+    """
     api.query()
     json_ = api.get_json
     return json_
 
 
-def test(resp):
+def json_to_html(resp):
     """Pass in JSON and return html table."""
     return json2html.convert(json=resp)
 
 
 def save_output(html):
-    with open('test.html', 'w') as f:
+    """Creates html file for storing html table object.
+
+    Minimal product for testing purposes.
+    Could generate name based on day, product, region etc; append; prettify
+    table with css etc.
+
+    :return html file containing table data.
+    """
+    with open('table.html', 'w') as f:
         for line in html:
             f.write(line)
 
